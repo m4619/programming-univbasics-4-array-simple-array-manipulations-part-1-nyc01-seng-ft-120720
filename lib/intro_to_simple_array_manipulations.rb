47ccb65e010e237ebcaeea65afd486ade80db8d4
Miguel  Arreola <marreola4619@gmail.com>
2020-11-06 18:21:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,5 +21,5 @@ end
 
 def using_unshift(array)
   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  
+  im_so_over_this_city = my_favorite_cities.shift
 end
