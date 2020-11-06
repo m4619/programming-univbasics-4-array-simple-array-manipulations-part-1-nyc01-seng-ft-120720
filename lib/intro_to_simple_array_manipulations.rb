a49adb685928b0f62937f1af61e007904df37b68
Miguel  Arreola <marreola4619@gmail.com>
2020-11-06 18:22:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,7 +19,7 @@ def pop_with_args(array)
   small_dogs = dog_breeds.pop(2) 
 end
 
-def using_unshift(array)
+def using_shift(array)
   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
   im_so_over_this_city = my_favorite_cities.shift
 end
