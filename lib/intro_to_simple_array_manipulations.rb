c9a1a618dadbc07772481b349a3fda71ef2710f6
Miguel  Arreola <marreola4619@gmail.com>
2020-11-06 18:26:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,5 +26,5 @@ end
 
 def shift_with_args(array)
   ice_cream = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  @ice_cream.shift(2)
+  @ice_cream_brands.shift(2)
 end
