7272814b0867d492d931ebb214c1d5e71da6f651
Miguel  Arreola <marreola4619@gmail.com>
2020-11-06 16:50:37 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,11 @@
+require 'pry'
+def using_push(array, string)
+   colors = ["red", "orange", "yellow", "green", "blue", "indigo"]
+   colors.push("violet")
+end
+
+def using_unshift(array, string)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  bouroughs_in_nyc.unshift("Staten Island")
+  binding.pry
+end
